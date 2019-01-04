205b0f01463ce72fda95727d926ab7a18b69faae
mynameisjenn <jenniferlynnhatton@gmail.com>
2019-01-03 21:47:59 -0600

    practiced creating toggle event

---
+++ Assigments/toggle.js
@@ -0,0 +1,23 @@
+var button = document.querySelector("button");
+// var body = document.querySelector("body");
+// var isRed = false;
+
+button.addEventListener("click", function() {
+	document.body.classList.toggle("red");
+})
+
+
+
+
+
+
+// button.addEventListener("click", toggleColor);
+
+// function toggleColor() {
+// 	if(isRed){
+// 		body.style.background = "white";
+// 	} else {
+// 		body.style.background = "red";
+// 	}
+// 	isRed = !isRed;
+// };
